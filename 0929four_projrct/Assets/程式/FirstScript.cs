f87c34b5c70a01c383807ce1a8be8ea2d8b887a2
ciq109051061 <109051061@gm.asia.edu.tw>
2022-10-13 16:01:54 +0800

    1013
    程式

---
+++ 0929four_projrct/Assets/程式/FirstScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Blythe
+{
+    /// <summary>
+    /// 第一個程式
+    /// </summary>
+    public class FirstScript : MonoBehaviour
+    {
+        #region 註解區域
+        // Start is called before the first frame update
+        
+
+        #endregion
+        #region 事件區域
+        private void Awake()
+        {
+            
+        }
+        private void Start()
+        {
+            
+        }
+        #endregion
+
+    }
+
+}
